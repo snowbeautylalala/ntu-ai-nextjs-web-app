8057533e8e797eaf0a0e174afa2a721a4e354885
snowbeautylalala <elfaquarsmile@gmail.com>
2024-06-23 11:59:18 +0800

    first deploy

---
+++ app/api/get-image-gen-list-from-db/route.js
@@ -0,0 +1,18 @@
+import db from "@/services/firebase-db";
+
+export async function GET() {
+    const dataList = [];
+    // 從Firestore(db)取得集合內的全部文件
+    const docList = await db.collection("vocab-gen-list").orderBy("createdAt", "desc").get();
+    // 逐一取出文件
+    docList.forEach(doc => {
+        const data = doc.data();
+        // doc.data()將文件轉為JS物件
+        data.id = doc.id;
+        // 將物件加到 dataList 內
+        dataList.push(data);
+    });
+    return Response.json({
+        dataList
+    });
+}
---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+
+};
+
+export default nextConfig;
